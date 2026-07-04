a8631315c113c9969c69e852bd436de259ece523
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:07 +0100

    Add sections/CenterLight.tsx

---
+++ sections/CenterLight.tsx
@@ -0,0 +1,7 @@
+// CenterLight — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+
+export default function CenterLight() {
+  return (
+    <div className="o-W1aXexQYM" />
+  )
+}
