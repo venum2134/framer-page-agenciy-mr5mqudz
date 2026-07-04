af5a10552252e0363750fdf7735e2c9b81b3e323
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:09 +0100

    Add sections/FooterSection.tsx

---
+++ sections/FooterSection.tsx
@@ -0,0 +1,11 @@
+// FooterSection — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import ResponsiveVariant from "../lib/ResponsiveVariant"
+import FooterSection2 from "../components/FooterSection2.js"
+
+export default function FooterSection() {
+  return (
+    <div className="o-mNRzS4pH4">
+      <ResponsiveVariant Component={FooterSection2} entries={[{"query":"(min-width: 810px) and (max-width: 1439.98px)","variant":"MhQD5lTRd"},{"query":"(max-width: 809.98px)","variant":"rvw4L8Vjw"}]} fallback={"kNCfE5J3E"} style={{ width: "100%" }} />
+    </div>
+  )
+}
