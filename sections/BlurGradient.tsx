70fb25c9b6b71350c07d452d7861fe91e04ba118
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:07 +0100

    Add sections/BlurGradient.tsx

---
+++ sections/BlurGradient.tsx
@@ -0,0 +1,10 @@
+// BlurGradient — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import BlurGradient2 from "../components/BlurGradient2.js"
+
+export default function BlurGradient() {
+  return (
+    <div className="o-G8K6bi5fQ">
+      <BlurGradient2 blur={7} borderRadius="0px" direction="to bottom" style={{ width: "100%" }} />
+    </div>
+  )
+}
