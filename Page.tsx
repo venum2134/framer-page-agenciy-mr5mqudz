dfb9d830674bf4a879958b07fa48ae81dc5c5eac
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:03 +0100

    Add Page.tsx

---
+++ Page.tsx
@@ -0,0 +1,49 @@
+// Page — Agenciy - Creative Agency Framer Template (copy) · page /
+// Généré par Obsidian Export (pipeline v2) le 04/07/2026
+import "./styles.css"
+import "./page.css"
+import BG from "./sections/BG"
+import HeaderSection from "./sections/HeaderSection"
+import CenterLight from "./sections/CenterLight"
+import N3DBall from "./sections/N3DBall"
+import Video from "./sections/Video"
+import HeroSection from "./sections/HeroSection"
+import ServiceSection from "./sections/ServiceSection"
+import AboutSection from "./sections/AboutSection"
+import ProjectSection from "./sections/ProjectSection"
+import ProcessSection from "./sections/ProcessSection"
+import AwardsSection from "./sections/AwardsSection"
+import TeamSection from "./sections/TeamSection"
+import PricingSection from "./sections/PricingSection"
+import TestimonialSection from "./sections/TestimonialSection"
+import FAQSection from "./sections/FAQSection"
+import ContactSection from "./sections/ContactSection"
+import SmoothScroll from "./sections/SmoothScroll"
+import BlurGradient from "./sections/BlurGradient"
+import FooterSection from "./sections/FooterSection"
+
+export default function Page() {
+  return (
+    <div className="o-root">
+      <BG />
+      <HeaderSection />
+      <CenterLight />
+      <N3DBall />
+      <Video />
+      <HeroSection />
+      <ServiceSection />
+      <AboutSection />
+      <ProjectSection />
+      <ProcessSection />
+      <AwardsSection />
+      <TeamSection />
+      <PricingSection />
+      <TestimonialSection />
+      <FAQSection />
+      <ContactSection />
+      <SmoothScroll />
+      <BlurGradient />
+      <FooterSection />
+    </div>
+  )
+}
