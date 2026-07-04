b3ff5b51b04943a268947d7a4a82e8e0118c86d9
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:09 +0100

    Add sections/FAQSection.tsx

---
+++ sections/FAQSection.tsx
@@ -0,0 +1,11 @@
+// FAQSection — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import ResponsiveVariant from "../lib/ResponsiveVariant"
+import FAQSection2 from "../components/FAQSection2.js"
+
+export default function FAQSection() {
+  return (
+    <div className="o-J_mkRK4po">
+      <ResponsiveVariant Component={FAQSection2} entries={[{"query":"(min-width: 810px) and (max-width: 1439.98px)","variant":"Oi1GtQZyU"},{"query":"(max-width: 809.98px)","variant":"ymNleH1oF"}]} fallback={"GfdU_kGhg"} style={{ width: "100%" }} />
+    </div>
+  )
+}
