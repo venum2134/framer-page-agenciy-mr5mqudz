fddb3a4b9556b589ba89e5ef07b5ffc118c1b2a5
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:08 +0100

    Add sections/ContactSection.tsx

---
+++ sections/ContactSection.tsx
@@ -0,0 +1,11 @@
+// ContactSection — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import ResponsiveVariant from "../lib/ResponsiveVariant"
+import ContactSection2 from "../components/ContactSection2.js"
+
+export default function ContactSection() {
+  return (
+    <div className="o-kUKNEZd8i">
+      <ResponsiveVariant Component={ContactSection2} entries={[{"query":"(min-width: 810px) and (max-width: 1439.98px)","variant":"k4J1IvcZQ"},{"query":"(max-width: 809.98px)","variant":"zRp_sdJwn"}]} fallback={"eRSpskOKc"} style={{ width: "100%" }} />
+    </div>
+  )
+}
