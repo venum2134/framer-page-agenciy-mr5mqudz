bae37a365f611b8e544c6d98f0ade07232d87d87
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:12 +0100

    Add sections/N3DBall.tsx

---
+++ sections/N3DBall.tsx
@@ -0,0 +1,8 @@
+// N3DBall — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import { motion } from "framer-motion"
+
+export default function N3DBall() {
+  return (
+    <motion.div className="o-f8DtNbUBD" role="img" aria-label="hero image" animate={{ x: 1, y: -10 }} transition={{ type: "tween", ease: [0.44, 0, 0.56, 1], duration: 1, delay: 0, repeat: Infinity, repeatType: "mirror" }} />
+  )
+}
