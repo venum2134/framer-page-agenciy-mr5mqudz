2a6b633ca7876d3b3a46374a16b63c440520990e
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:10 +0100

    Add sections/HeaderSection.tsx

---
+++ sections/HeaderSection.tsx
@@ -0,0 +1,11 @@
+// HeaderSection — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import ResponsiveVariant from "../lib/ResponsiveVariant"
+import HeaderSection2 from "../components/HeaderSection2.js"
+
+export default function HeaderSection() {
+  return (
+    <div className="o-mljptPHxN">
+      <ResponsiveVariant Component={HeaderSection2} entries={[{"query":"(min-width: 810px) and (max-width: 1439.98px)","variant":"EahEbt04x"},{"query":"(max-width: 809.98px)","variant":"EahEbt04x"}]} fallback={"Jb9jDnDUp"} style={{ width: "100%" }} />
+    </div>
+  )
+}
