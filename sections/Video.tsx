c63556f2c99e75632dd88f13881e713bc427458a
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:17 +0100

    Add sections/Video.tsx

---
+++ sections/Video.tsx
@@ -0,0 +1,10 @@
+// Video — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import Video2 from "../components/Video2.js"
+
+export default function Video() {
+  return (
+    <div className="o-qoUdVTmRi">
+      <Video2 srcType="Upload" srcUrl="https://framerusercontent.com/assets/MLWPbW1dUQawJLhhun3dBwpgJak.mp4" srcFile={{"id":"shkethPArotkZlMS0MbrQ6WEC78.mp4","url":"https://framerusercontent.com/assets/shkethPArotkZlMS0MbrQ6WEC78.mp4","extension":"mp4","src":"https://framerusercontent.com/assets/shkethPArotkZlMS0MbrQ6WEC78.mp4","alt":""}} playing={true} posterEnabled={false} backgroundColor="rgba(255, 255, 255, 0)" startTime={0} loop={true} objectFit="fill" controls={false} muted={true} volume={25} style={{ width: "100%" }} />
+    </div>
+  )
+}
