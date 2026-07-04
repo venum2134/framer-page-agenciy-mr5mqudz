fc8e169c0541a3295d3e177cb68b2f88a418d79d
venum2134 <venumakemi@gmail.com>
2026-07-04 01:35:15 +0100

    Add sections/SmoothScroll.tsx

---
+++ sections/SmoothScroll.tsx
@@ -0,0 +1,10 @@
+// SmoothScroll — généré par Obsidian Export depuis Agenciy - Creative Agency Framer Template (copy)
+import SmoothScroll2 from "../components/SmoothScroll2.js"
+
+export default function SmoothScroll() {
+  return (
+    <div className="o-DdnbyoogS">
+      <SmoothScroll2 intensity={15} />
+    </div>
+  )
+}
